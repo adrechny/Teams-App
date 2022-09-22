5b59759700f902795c51b9f45ff4bf9a4afb23cb
adrechny <adrechny311@gmail.com>
2022-09-21 19:10:12 -0500

    Added employee profile pictures and names

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-import Content from "./Content";
 import Footer from "./Footer";
+import Employees from "./Employees";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Content />
+      <Employees />
       <Footer />
     </div>
   );
