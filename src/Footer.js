c593d1772ea0c664468d09afc73ec914c1332ec4
adrechny <adrechny311@gmail.com>
2022-09-28 20:30:44 -0500

    Added info to footer

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,16 @@ export default function Footer() {
       <div className="row justify-content-center mt-3 mb-4">
         <div className="col-8">
           <h5>Team Member Allocation - {today.getFullYear()}</h5>
+          <div className="info">
+            This project was coded by Allison Drechny and is{" "}
+            <a
+              href="https://github.com/adrechny/Teams-App"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+          </div>
         </div>
       </div>
     </footer>
